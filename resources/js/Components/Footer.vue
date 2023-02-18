4a73012823a07b5309de892a4d2557a8ebbdc776
JBiezais <juris.oskars@gmail.com>
2023-02-19 00:01:52 +0200

    displayment fix

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -13,7 +13,7 @@
         <div class="md:text-right ml-auto text-gray-200 flex flex-col my-auto space-y-2">
             <a href="/files/RSU%20SP_Privātuma%20politika.pdf" target="_blank">Datu izmantošana</a>
             <h1>Alise Rozentāle</h1>
-            <div class="flex space-x-5">
+            <div class="flex flex-col">
                 <a href="tel:+37127727419">+371 27 727 419</a>
                 <a href="mailto:aliseagnese.rozentale@rsu.lv">AliseAgnese.Rozentale@rsu.lv</a>
             </div>

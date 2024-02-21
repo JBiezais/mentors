4e04f4fd807ffbc74f69c923c5e75dab41c0560f
Juris Biezais <jurisbiezais@Juriss-MacBook-Air.local>
2024-02-21 18:28:06 +0200

    Mentors | Add user

---
+++ database/migrations/2024_02_21_162505_add_user_to_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        User::query()->create([
+            'name' => 'Luīze Driķīte',
+            'email' => 'luize.drikite@rsu.lv',
+            'password' => bcrypt('BurkaniZeme1'),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+    }
+};
